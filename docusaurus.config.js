143ff753db44eeb46972a7ad9fa77c73fa95da6a
Adam Gallard <adam@thegallards.com>
2025-08-12 20:54:03 +1000

    updated the edit URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
             {
                 docs: {
                     sidebarPath: './sidebars.js',
-                    editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    editUrl: 'https://github.com/adamGallard/BelmontHandbook/edit/main/',
                 },
             },
         ],
